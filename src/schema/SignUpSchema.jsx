276028b1e59ded99b0a92ec4351381196fd091f0
Sutaenghhh <neocop7@gmail.com>
2025-09-02 16:30:08 +0900

    feature: 회원가입 유효성 검사 추가

--- src/schema/SignUpSchema.jsx
+++ src/schema/SignUpSchema.jsx
@@ -4,16 +4,17 @@ export const SignUpSchema = z.object({
   name: z.string().min(1, { message: '이름은 1자 이상이어야 합니다.' }),
   userId: z
     .string()
-    .min(4, { message: '영문 소문자/숫자 , 4~16자 이어야 합니다.' }),
+    .min(4, '아이디는 4자 이상이어야 합니다')
+    .max(20, '아이디는 20자 이하여야 합니다')
+    .regex(/^[a-zA-Z0-9]+$/, '아이디는 특수문자, 공백 없이 입력해주세요'),
   email: z.string().email({ message: '이메일 형식이 올바르지 않습니다.' }),
   password: z
     .string()
-    .min(8, { message: '영문 대문자/숫자 , 8~16자 이어야 합니다.' }),
-  passwordConfirm: z
-    .string()
-    .min(8, { message: '영문 대문자/숫자 , 8~16자 이어야 합니다.' }),
-  address: z.string().min(1, { message: '주소는 1자 이상이어야 합니다.' }),
-  addressDetail: z
-    .string()
-    .min(1, { message: '상세주소는 1자 이상이어야 합니다.' }),
+    .min(8, '비밀번호는 8자 이상이어야 합니다')
+    .max(20, '비밀번호는 20자 이하여야 합니다')
+    .regex(
+      /^(?=.*[a-z])(?=.*[A-Z])|(?=.*[a-z])(?=.*[0-9])|(?=.*[a-z])(?=.*[!@#$%^&*])|(?=.*[A-Z])(?=.*[0-9])|(?=.*[A-Z])(?=.*[!@#$%^&*])|(?=.*[0-9])(?=.*[!@#$%^&*])/,
+      '비밀번호는 영문 대소문자, 숫자, 특수문자 중 2개 이상을 포함해야 합니다',
+    ),
+  confirmPassword: z.string().min(1, '비밀번호 확인을 입력하세요.'),
 });
