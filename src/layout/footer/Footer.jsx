6cc5efa000f0026ea8070060298647862b0f06a9
Sutaenghhh <neocop7@gmail.com>
2025-09-09 14:44:20 +0900

    refactor: footer contents:

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -12,9 +12,10 @@ export default function Footer() {
       <div className='container mx-auto px-4'>
         <div className='flex flex-col items-start  font-pretendard'>
           <img src={mongsomLogo} alt='Mongsom Logo' className='h-4 w-auto' />
-          <p className='text-black-100 font-pretendard text-sm'>(주)몽솜</p>
+          <p className='text-black-100 font-pretendard text-sm'>
+            주식회사 몽솜
+          </p>
         </div>
-
         <ul className='flex gap-4 pt-3 text-sm font-pretendard'>
           <li>
             <p>대표자: 김준겸</p>
@@ -22,7 +23,9 @@ export default function Footer() {
           <li>
             <p>주소: 부산광역시 강서구 녹산산단361로 42,3층(송정동)</p>
           </li>
-          <li>{/* <p>고객센터: 02-1234-5678</p> */}</li>
+          <li>
+            <p>고객센터: 010-9364-7630</p>
+          </li>
         </ul>
 
         <ul className='flex gap-4 text-sm font-pretendard'>
