6f73cf3d5345b778a919cb4065d75dead4958eaa
Sutaenghhh <neocop7@gmail.com>
2025-11-21 16:35:04 +0900

    refactor: 베스트리뷰 수정 및 브랜드 스토리 이미지 변경

--- src/shop/data/BestReview.jsx
+++ src/shop/data/BestReview.jsx
@@ -2,23 +2,23 @@ export const bestReview = [
   {
     id: 1,
     image:
-      'https://firebasestorage.googleapis.com/v0/b/mongsom-22c9b.firebasestorage.app/o/review%2F%E1%84%80%E1%85%A9%E1%86%AF%E1%84%91%E1%85%B3%E1%84%80%E1%85%A9%E1%86%BC3.jpeg?alt=media&token=e783a8b2-296a-4f2d-9b40-d8cae49386bd',
-    title: '각인 골프공 선물세트 6개입 (화이트) - 박스포장',
-    description: '기업 판촉 선물',
+      'https://firebasestorage.googleapis.com/v0/b/mongsom-22c9b.firebasestorage.app/o/review%2F%E1%84%85%E1%85%B5%E1%84%87%E1%85%B2_%E1%84%92%E1%85%A2%E1%86%AB%E1%84%83%E1%85%B3%E1%84%8F%E1%85%B3%E1%84%85%E1%85%B5%E1%86%B71.jpg?alt=media&token=346ac8b7-22fe-463c-8be9-1e76e3a99d46',
+    title: '핸드크림 선물세트',
+    description: '결혼식 답례품',
   },
   {
     id: 2,
     image:
-      'https://firebasestorage.googleapis.com/v0/b/mongsom-22c9b.firebasestorage.app/o/review%2F%E1%84%83%E1%85%B5%E1%84%91%E1%85%B2%E1%84%8C%E1%85%A5.jpeg?alt=media&token=6b0f7c4e-0fa2-4304-be8b-4ab6e1f5fb62',
-    description: '결혼식 답례품',
-    title: '디퓨저 선물세트 (화이트) - 박스포장',
+      'https://firebasestorage.googleapis.com/v0/b/mongsom-22c9b.firebasestorage.app/o/review%2F%E1%84%85%E1%85%B5%E1%84%87%E1%85%B2_%E1%84%92%E1%85%A2%E1%86%AB%E1%84%83%E1%85%B3%E1%84%8F%E1%85%B3%E1%84%85%E1%85%B5%E1%86%B72.jpg?alt=media&token=feda1e90-88ae-494f-8a79-f81c35813813',
+    description: '핸드크림',
+    title: '생일선물',
   },
   {
     id: 3,
     image:
-      'https://firebasestorage.googleapis.com/v0/b/mongsom-22c9b.firebasestorage.app/o/review%2F%E1%84%8F%E1%85%B5%E1%84%85%E1%85%B5%E1%86%BC2.jpeg?alt=media&token=08138c6a-83d0-4656-b22a-3e3c8e0fe823',
+      'https://firebasestorage.googleapis.com/v0/b/mongsom-22c9b.firebasestorage.app/o/review%2F%E1%84%85%E1%85%B5%E1%84%87%E1%85%B2_%E1%84%92%E1%85%A2%E1%86%BC%E1%84%8C%E1%85%AE1.jpg?alt=media&token=279ffd6d-e4df-41d6-9c6f-3e37f5f56b9a',
 
-    title: '명품 키링 선물세트  - 박스포장',
-    description: '돌잔치 답례품',
+    title: '다회용 행주',
+    description: '개인 구매',
   },
 ];
